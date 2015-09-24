c6c7c90b81d8883a3d7080c749b4f69513804802
Aubrey Keus <aekeus@gmail.com>
2015-09-24 23:44:29 +0000

    console.log cleanup

--- bin/api.js
+++ bin/api.js
@@ -1,21 +1,19 @@
-var fs   = require('fs');
-var path = require('path');
-var Hapi = require('hapi');
-var glob = require('glob');
+var fs     = require('fs');
+var path   = require('path');
+var Hapi   = require('hapi');
+var glob   = require('glob');
 var assert = require('assert');
+
 var db = require('monk')('localhost/accounting');
 
 var config = JSON.parse(fs.readFileSync(path.join('etc', 'dev.json'), 'utf-8'));
-
-console.log(config);
 
 var server = new Hapi.Server();
 server.connection({ port: config.port });
 
 glob(path.join('lib', 'api*.js'), function(err, api_files) {
-  console.log(api_files);
   api_files.forEach(function(filename) {
-    console.log(filename);
+    console.log(`Installing ${filename}`);
     // install the handlers
     require(path.join(__dirname, '..', filename)).setup(server, db);
   });
