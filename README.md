78dfe26d4952e1147d3424d39ac5cc967dac8fb0
Aubrey Keus <aekeus@gmail.com>
2015-09-30 23:41:48 +0000

    Logging and static directory handling

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ hapi.js / MongoDB RESTful starter project
 
 Requires babel to compile ECMAScript 2015 -> ES5 JavaScript
 
-To build and start: `make && sudo npm start`
+To build and start: `npm install && make && sudo npm start`
 
 
--- bin/api.js
+++ bin/api.js
@@ -11,6 +11,28 @@ var config = JSON.parse(fs.readFileSync(path.join('etc', 'dev.json'), 'utf-8'));
 var server = new Hapi.Server();
 server.connection({ port: config.port });
 
+// super simple logging (http://stackoverflow.com/users/115493/m01 - http://stackoverflow.com/questions/29746259/how-to-log-all-requests-made-to-a-hapi-server)
+server.on('response', function (request) {
+    console.log(request.info.remoteAddress + ': ' + request.response.statusCode + ' ' + request.method.toUpperCase() + ' ' + request.url.path );
+});
+
+// setup a static directory handler
+var setup_static_directory = function(server, config, done) {
+  server.register(require('inert'), function (err) {
+    console.log("Setting up static directory in " + path.join(__dirname, '..', config.static));
+    server.route({
+      method: 'GET',
+      path: '/{param*}',
+      handler: {
+        directory: {
+            path: path.join(__dirname, '..', config.static)
+        }
+      }
+    });
+    done(err);
+  });
+};
+
 glob(path.join('lib', 'api*.js'), function(err, api_files) {
   api_files.forEach(function(filename) {
     console.log(`Installing ${filename}`);
@@ -18,9 +40,16 @@ glob(path.join('lib', 'api*.js'), function(err, api_files) {
     require(path.join(__dirname, '..', filename)).setup(server, db);
   });
 
-  server.start(function(err) {
+  // setup the static directory
+  setup_static_directory(server, config, function(err) {
     assert.equal(null, err);
-    console.log("server started");
+
+    // startup the server
+    server.start(function(err) {
+      assert.equal(null, err);
+      console.log("server started");
+    });
+
   });
 
 });
